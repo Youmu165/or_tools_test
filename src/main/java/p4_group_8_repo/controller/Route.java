979a92aa7285bb3ba1e3106927a7dd648f0d1b43
Keyu MEI <scykm2@nottingham.ac.uk>
2025-01-30 13:49:13 +0000

    add or-tools, some problems remaining

--- src/main/java/p4_group_8_repo/controller/Route.java
+++ src/main/java/p4_group_8_repo/controller/Route.java
@@ -2,6 +2,7 @@ package p4_group_8_repo.controller;
 
 import com.google.ortools.Loader;
 import com.google.ortools.constraintsolver.*;
+import com.google.protobuf.Duration;
 import org.apache.commons.math3.ml.distance.EuclideanDistance;
 import p4_group_8_repo.utils.csvUtil;
 
@@ -24,6 +25,8 @@ public class Route {
             });
         }
 
+        //calculate the matrix of distance
+
         int size = coordinates.size();
         double[][] distanceMatrix = new double[size][size];
         EuclideanDistance distanceCalculator = new EuclideanDistance();
@@ -33,15 +36,99 @@ public class Route {
                 distanceMatrix[i][j] = (i == j) ? 0 : distanceCalculator.compute(coordinates.get(i), coordinates.get(j));
             }
         }
-
-        List<String[]> routeData = new ArrayList<>();
-        routeData.add(new String[]{"from_cluster", "to_cluster", "distance"});
-        for (int i = 0; i < size; i++) {
-            for (int j = i + 1; j < size; j++) {
-                routeData.add(new String[]{String.valueOf(i), String.valueOf(j), String.valueOf(distanceMatrix[i][j])});
+        double maxDistance = 0.0;
+        for (int i = 0; i < distanceMatrix.length; i++) {
+            for (int j = 0; j < distanceMatrix[i].length; j++) {
+                if (distanceMatrix[i][j] > maxDistance) {
+                    maxDistance = distanceMatrix[i][j];
+                }
             }
         }
+        if (maxDistance > 0) {
+            for (int i = 0; i < distanceMatrix.length; i++) {
+                for (int j = 0; j < distanceMatrix[i].length; j++) {
+                    distanceMatrix[i][j] = (distanceMatrix[i][j] / maxDistance) * 1000;
+                }
+            }
+        }
+//        for (int i = 0; i < distanceMatrix.length; i++) {
+//            for (int j = 0; j < distanceMatrix[i].length; j++) {
+//                System.out.print(distanceMatrix[i][j] + " ");
+//            }
+//            System.out.println();
+//        }
+
+        List<Integer> optimalRoute = solveTSP(distanceMatrix);
+
+        if (optimalRoute == null) {
+            System.err.println(" ERROR: Not enough optimal routes to compute routes.");
+            return;
+        }
+
+        List <String[]> routeData = new ArrayList<>();
+        routeData.add(new String[]{"from_cluster", "to_cluster", "distance"});
+
+        for (int i = 0; i < optimalRoute.size() - 1; i++) {
+            int from = optimalRoute.get(i);
+            int to = optimalRoute.get(i + 1);
+            routeData.add(new String[]{String.valueOf(from), String.valueOf(to), String.valueOf(distanceMatrix[from][to])});
+
+        }
+
         csvUtil.writeCSV(routesCSV, routeData);
+        System.out.println("Routes computed successfully.");
+//        List<String[]> routeData = new ArrayList<>();
+//        routeData.add(new String[]{"from_cluster", "to_cluster", "distance"});
+//        for (int i = 0; i < size; i++) {
+//            for (int j = i + 1; j < size; j++) {
+//                routeData.add(new String[]{String.valueOf(i), String.valueOf(j), String.valueOf(distanceMatrix[i][j])});
+//            }
+//        }
+//        csvUtil.writeCSV(routesCSV, routeData);
+    }
+
+    public static List<Integer> solveTSP(double[][] distanceMatrix) {
+        Loader.loadNativeLibraries();
+
+        int numNodes = distanceMatrix.length;
+        RoutingIndexManager manager = new RoutingIndexManager(numNodes, 1, 0);
+        RoutingModel routing = new RoutingModel(manager);
+
+        int transitCallbackIndex = routing.registerTransitCallback((long fromIndex, long toIndex) ->{
+            int fromNode = manager.indexToNode(fromIndex);
+            int toNode = manager.indexToNode(toIndex);
+            return (long) distanceMatrix[fromNode][toNode];
+
+                });
+
+
+        routing.setArcCostEvaluatorOfAllVehicles(transitCallbackIndex);
+
+        //routing.addVariableMinimizedByFinalizer(routing.costVar());
+       // routing.setAllowEmptyRoutes(true);
+        RoutingSearchParameters searchParameters = RoutingSearchParameters.newBuilder()
+                .setFirstSolutionStrategy(FirstSolutionStrategy.Value.PATH_CHEAPEST_ARC)
+                .setLocalSearchMetaheuristic(LocalSearchMetaheuristic.Value.GUIDED_LOCAL_SEARCH)
+                .setSolutionLimit(1000)
+                .setTimeLimit(Duration.newBuilder().setSeconds(5).build())
+                .build();
+
+        Assignment solution = routing.solveWithParameters(searchParameters);
+        if (solution == null) {
+            System.err.println("ERROR: No optimal route found. OR-Tools failed to solve TSP.");
+            return null;
+        }
+
+        List<Integer> route = new ArrayList<>();
+        long index = routing.start(0);
+        while (!routing.isEnd(index)) {
+            route.add(manager.indexToNode(index));
+            index = solution.value(routing.nextVar(index));
+
+        }
+        route.add(0);
+        return route;
+
     }
 
     public static void assignCargoToClusters(String inputCSV, String cargoCSV) {
